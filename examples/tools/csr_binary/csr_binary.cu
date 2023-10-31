c5cbfaf6a0bd3c8c5c0944e6c7689b49cc85279b
Subhajit Sahu <wolfram77@gmail.com>
2023-10-31 09:48:28 +0530

    :bug: update csr binary to report graph load/write time

--- examples/tools/csr_binary/csr_binary.cu
+++ examples/tools/csr_binary/csr_binary.cu
@@ -1,4 +1,5 @@
 #include <gunrock/algorithms/algorithms.hxx>
+#include <chrono>
 
 using namespace gunrock;
 using namespace memory;
@@ -24,10 +25,15 @@ void mtx2bin(int num_arguments, char** argument_array) {
 
   io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
 
+  auto t0 = std::chrono::high_resolution_clock::now();
   using csr_t =
       format::csr_t<memory::memory_space_t::device, vertex_t, edge_t, weight_t>;
   csr_t csr;
   csr.from_coo(mm.load(inpath));
+  auto t1 = std::chrono::high_resolution_clock::now();
+  std::cout << "graph loading time = "
+    << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count()
+    << "ms\n";
 
   std::cout << "csr.number_of_rows     = " << csr.number_of_rows << std::endl;
   std::cout << "csr.number_of_columns  = " << csr.number_of_columns
@@ -36,7 +42,12 @@ void mtx2bin(int num_arguments, char** argument_array) {
             << std::endl;
   std::cout << "writing to             = " << outpath << std::endl;
 
+  auto t2 = std::chrono::high_resolution_clock::now();
   csr.write_binary(outpath);
+  auto t3 = std::chrono::high_resolution_clock::now();
+  std::cout << "graph writing time = "
+    << std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count()
+    << "ms\n";
 }
 
 int main(int argc, char** argv) {
